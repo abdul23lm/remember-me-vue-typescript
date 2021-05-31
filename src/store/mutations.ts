1010c69c81375ac549405b7ee7a985d7f5beb7b4
Abdul Latif Munjiat <58551842+abdul23lm@users.noreply.github.com>
2021-05-31 23:53:13 +0700

    Update mutations.ts

--- src/store/mutations.ts
+++ src/store/mutations.ts
@@ -18,5 +18,19 @@ export type Mutations = {
   [MutationType.SetLoading](state: State, value: boolean): void
 }
 
-
+export const mutations: MutationTree<State> & Mutations = {
+  [MutationType.CreateItem](state, item) {
+    state.items.unshift(item)
+  },
+  [MutationType.SetItems](state, items) {
+    state.items = items
+  },
+  [MutationType.CompleteItem](state, newItem) {
+    const item = state.items.findIndex(s => s.id === newItem.id)
+    if (item === -1) return
+    state.items[item] = { ...state.items[item], ...newItem }
+  },
+  [MutationType.SetLoading](state, value) {
+    state.loading = value
+  }
 }
